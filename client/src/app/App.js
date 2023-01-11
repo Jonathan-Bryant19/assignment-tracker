2a947465d449d1dba62cf57a49007475149a0bf9
Jonathan Bryant <spectrum19tech@gmail.com>
2023-01-11 12:15:18 -0800

    React setup

---
+++ client/src/app/App.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react"
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import Home from "../components/Home"
+import Login from "../components/Login"
+import Signup from "../components/Signup"
+import NavBar from "../components/NavBar"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    fetch("/hello")
+      .then((r) => r.json())
+      .then((data) => setCount(data.count))
+  }, [])
+
+  return (
+    <Router>
+      <Route >
+        <NavBar />
+      </Route>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route path='/signup'>
+          <Signup />
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
+
+export default App
