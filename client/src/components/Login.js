a48072c06a3d65c12905c9243f78c155f35df98c
Jonathan Bryant <spectrum19tech@gmail.com>
2023-01-20 22:16:32 -0800

    Begin adding login functionality.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -7,32 +7,30 @@ function Login() {
   const [errors, setErrors] = useState(null)
   const [user, setUser] = useState(null)
   const navigate = useNavigate()
-  // This is code that needs to be fully reviewed. Was copy/pasted from Signup.  
+  
   function onLogin(e) {
     e.preventDefault()
-    // fetch('/signup', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //   },
-    //   body: JSON.stringify({
-    //     username,
-    //     email,
-    //     password,
-    //     password_confirmation: passwordConfirmation
-    //   })
-    // }).then(r => {
-    //   if (r.ok) {
-    //     r.json().then(user => setUser(user))
-    //     console.log(user)
-    //     navigate('/', {state: user})
-    //   } else if (r.status === 422) {
-    //     r.json().then(json => setErrors(json.errors))
-    //     console.log(errors)
-    //   }
-    // })
+    fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username,        
+        password
+      })
+    }).then(r => {
+      if (r.ok) {
+        r.json().then(user => setUser(user))
+        console.log(user)
+        navigate('/', {state: user})
+      } else if (r.status === 422) {
+        r.json().then(json => setErrors(json.errors))
+        console.log(errors)
+      }
+    })
   }  
-  
+  console.log(username, password)
   return (    
     <div className='container'>
       <div className='row mt-3'>
@@ -42,19 +40,19 @@ function Login() {
           <form className='col'>
               <div className="my-3">
                   <label for="exampleInputEmail1" className="form-label">Username</label>
-                  <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                  <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={setUsername}/>
                   <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
               </div>
               <div className="mb-3">
                   <label for="exampleInputPassword1" className="form-label">Password</label>
-                  <input type="password" className="form-control" id="exampleInputPassword1" />
+                  <input type="password" className="form-control" id="exampleInputPassword1" onChange={setPassword}/>
               </div>
               <div className="mb-3 form-check">
                   <input type="checkbox" className="form-check-input" id="exampleCheck1" />
                   <label className="form-check-label" for="exampleCheck1">Check me out</label>
               </div>
               <button type="submit" className="btn btn-primary" onClick={onLogin}>Submit</button>
-          </form>
+          </form> 
       </div>
     </div>
   )
